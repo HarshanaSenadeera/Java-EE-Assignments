2a09f0d67f59a0c471729ea72301fe89b2875fa4
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-08-19 19:01:02 +0530

    Done Add and load all

--- src/lk/ijse/json/servlet/CustomerServlet.java
+++ src/lk/ijse/json/servlet/CustomerServlet.java
@@ -54,18 +54,100 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String cusID = req.getParameter("cusID");
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
 
+
+        resp.addHeader("Content-Type","application/json");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test3", "root", "1234");
+
+                    PreparedStatement pstm = connection.prepareStatement("insert into customer values(?,?,?)");
+                    pstm.setObject(1, cusID);
+                    pstm.setObject(2, cusName);
+                    pstm.setObject(3, cusAddress);
+//                    resp.addHeader("Content-Type","application/json");
+                    if (pstm.executeUpdate() > 0) {
+//                        resp.getWriter().println("Customer Added..!");
+
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("state","OK");
+                        objectBuilder.add("message","Successfully Added.....");
+                        objectBuilder.add("Data"," ");
+                        resp.getWriter().print(objectBuilder.build());
+                    }
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String cusID = req.getParameter("cusID");
+        String cusName = req.getParameter("cusName");
+        String cusAddress = req.getParameter("cusAddress");
 
+        resp.addHeader("Content-Type","application/json");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test3", "root", "1234");
+
+                    PreparedStatement pstm3 = connection.prepareStatement("update customer set name=?,address=? where id=?");
+                    pstm3.setObject(3, cusID);
+                    pstm3.setObject(1, cusName);
+                    pstm3.setObject(2, cusAddress);
+                    if (pstm3.executeUpdate() > 0) {
+                        resp.getWriter().println("Customer Updated..!");
+            }
+
+            /* resp.sendRedirect("/lk/ijse/jsp/pages/customer");*/
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String cusID = req.getParameter("cusID");
+
+        resp.addHeader("Content-Type","application/json");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test3", "root", "1234");
+
+                    PreparedStatement pstm2 = connection.prepareStatement("delete from customer where ID=?");
+                    pstm2.setObject(1, cusID);
+
+                    if (pstm2.executeUpdate() > 0) {
+
+                        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                        objectBuilder.add("state","OK");
+                        objectBuilder.add("message","Successfully Deleted.....");
+                        objectBuilder.add("Data"," ");
+                        resp.getWriter().print(objectBuilder.build());
+                    }
+
+            /* resp.sendRedirect("/lk/ijse/jsp/pages/customer");*/
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     }
-}
+
